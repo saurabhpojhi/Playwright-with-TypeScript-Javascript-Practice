b16f35abee1274591eb58ece7c8110f4a759f529
saurabhpojhi <saurabh.kumarjan10@gmail.com>
2026-08-20 00:28:33 +0530

    playwrgiht and javasecrpt code

---
+++ javaScript/Operator/Operator.ts
@@ -0,0 +1,13 @@
+
+/// Ternary/condition operator
+
+// ?:
+
+// Exp ? res1 :res2;
+
+//example 1
+
+ let a:number=100, b:number=200;
+
+ let res:number=(a>b)? a:b ;
+ console.log(res)
---
+++ tests/mytest.spec.ts
@@ -0,0 +1,25 @@
+import { test, expect } from "@playwright/test";
+
+/* // syntex
+
+test("title",()=>{
+   //step1 
+     //step12
+       //step3
+
+}); */
+
+test("verify the patge title", async ({page})=>{
+          await  page.goto("https://www.google.com/");
+     //.  await    const title : String=page.title();
+        //   console.log(title);
+         await  expect(page).toHaveTitle("Google");
+      //  await expect(page.getByRole('button',{name:'I am feeling lucky'})).toBeVisible();
+            //  await page.getByText('I am feeling lucky', { exact: true }) 
+            const feelingLucky = page.getByText("I am feeling lucky", {
+        exact: true
+    });
+ 
+    console.log(await feelingLucky.count());
+
+});  
